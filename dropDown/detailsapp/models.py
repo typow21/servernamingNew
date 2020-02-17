348e396789ec87895b47804315b4389f7da0fa87
Tyler <tuh26432@temple.edu>
2020-02-17 15:43:23 -0500

    Deleted drop down

--- dropDown/detailsapp/models.py
+++
@@ -1,9 +0,0 @@
-# Create your models here.
-
-from django.db import models
-
-class Server(models.Model):
-    OS = models.CharField(max_length = 100)
-    purpose = models.CharField(max_length = 100)
-    role = models.CharField(max_length = 100)
-    # name = os + purpose + role
